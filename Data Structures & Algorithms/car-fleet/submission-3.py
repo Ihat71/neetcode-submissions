efcb1d2380dd26c810abd9f80a9cca7172bb507d
Mohammed Najmaldeen <145692939+Ihat71@users.noreply.github.com>
2026-08-10 16:32:43 +0300

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        car_fleet = []
+        count=0
+
+        vector_list = list(zip(position, speed))
+        vector_list.sort(key=lambda tup: tup[0], reverse=True)
+
+        for x in vector_list:
+            time = (target-x[0]) / x[1]
+            if car_fleet:
+                if time <= car_fleet[0]:
+                    car_fleet.append(time)
+                else:
+                    car_fleet = []
+                    car_fleet.append(time)
+                    count += 1
+            if not car_fleet:
+                car_fleet.append(time)
+                count+=1
+
+
+        return count
