d04b5b468b3e79166e235c041b1c438f26c8a504
Mohammed Najmaldeen <145692939+Ihat71@users.noreply.github.com>
2026-08-12 12:33:14 +0300

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        punc = ['.', '?', '!', ',', ' ', "'", ':']
+        for p in punc:
+            s=s.replace(p, '')
+
+        last_index = len(s) - 1
+
+
+        for i in range(len(s)):
+            if s[i].lower() != s[last_index - i].lower():
+                return False
+
+        return True
