6e1a5ac43fe1373c72d8b167ea94e701c76f4332
Mohammed Najmaldeen <145692939+Ihat71@users.noreply.github.com>
2026-08-13 14:12:10 +0300

    Add: three-integer-sum - submission-35

---
+++ Data Structures & Algorithms/three-integer-sum/submission-35.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        output = []
+        seen = set()
+
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i-1]:
+                continue
+            target = -1 * num
+            j = i + 1
+            k = len(nums) - 1
+            while(j < k):
+                if nums[j] + nums[k] > target:
+                    k-=1
+                elif nums[j] + nums[k] < target:
+                    j += 1
+                elif nums[j] + nums[k] == target:
+                    # answer = (nums[i], nums[j], nums[k])
+                    #if you can prevent lookup this can get to sub 150 ms potentially
+                    # if answer not in seen:
+                    #     seen.add(answer)
+                    #     output.append(answer)
+                    # j += 1
+                    # k -= 1
+                    output.append([num, nums[j], nums[k]])
+                    j += 1
+                    while(nums[j] == nums[j-1] and j<k):
+                        j += 1
+
+
+        return output
+
