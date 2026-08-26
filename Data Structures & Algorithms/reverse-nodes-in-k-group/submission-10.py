28b828139668fbfb4b50676fca55a1911f324889
Mohammed Najmaldeen <145692939+Ihat71@users.noreply.github.com>
2026-08-26 13:09:30 +0300

    Add: reverse-nodes-in-k-group - submission-10

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-10.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        #I want to restart this problem rq
+
+        dummy = ListNode(-1, None)
+        tail = dummy
+        new_tail = None
+
+        curr = head
+        count=0
+
+        while curr:
+            count+=1
+            new_node = curr.next
+            if not tail.next:
+                new_tail = curr
+            if count == k:
+                self.reverseNode(tail, curr, k)
+                count = 0
+                tail = new_tail
+                tmp = new_node
+                break_bool = False
+                for _ in range(k-1):
+                    if not tmp or not tmp.next:
+                        tail.next = new_node
+                        break_bool = True
+                        break
+                    tmp = tmp.next
+                if break_bool:
+                    break
+            elif count < k:
+                self.reverseNode(tail, curr, k)
+            
+            curr = new_node
+        return dummy.next
+    def reverseNode(self, tail, curr, k, edge=False):
+        if not tail.next:
+            tail.next = curr
+            tmp = curr.next
+            curr.next = None
+                    
+        else:
+            curr.next = tail.next
+            tail.next = curr
+        return True
+            
